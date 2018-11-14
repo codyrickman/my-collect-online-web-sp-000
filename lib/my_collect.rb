d69eb7d20b7b7679a3dd30e547a73618d70507c0
Cody Rickman <cody@creativemindstudios.com>
2018-11-14 01:31:25 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   if block_given?
     i = 0
     new_array = []
-    while i < array.length
+    while i < array.length do |language|
       new_array << yield (language.split(" ").first)
     end
     new_array
