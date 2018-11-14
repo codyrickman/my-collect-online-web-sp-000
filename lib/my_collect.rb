9344ab868698bbd88cee33349e2cb352f876751a
Cody Rickman <cody@creativemindstudios.com>
2018-11-14 01:30:20 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    new_array = []
+    while i < array.length
+      new_array << yield language.split(" ").first
+    end
+    new_array
+  end
+end
 
