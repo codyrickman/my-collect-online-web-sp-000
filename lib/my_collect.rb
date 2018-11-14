0b98cf9840f774cdd5546ee980787c805528b9bf
Cody Rickman <cody@creativemindstudios.com>
2018-11-14 01:34:36 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
     i = 0
     new_array = []
     while (i < array.length)
-      new_array << yield (array[i].split(" ").first)
+      new_array << yield(array[i].split(" ").first)
       i += 1
     end
     new_array
